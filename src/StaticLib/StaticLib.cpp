4cd3da43a41d25cecfc1a6ab8d15f22bb39fc458
megutan30 <megameguroko@gmail.com>
2021-11-20 00:02:09 +0900

    模索中

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -55,6 +55,43 @@ static node* generate(int key, const char* value)
 	return p;
 }
 
+bool myadd(node* p, int key, const char* value)
+{
+	//チェック
+	if (p == NULL || key == NULL || value)return false;
+	
+	//同値
+	if (p->key == key)
+	{
+		node* x = p;
+		memcpy(x->value, p->value, sizeof(char) * 256);
+		free(p);
+		return true;
+	}
+
+	//Left再帰
+	if (key < p->key && p->left == NULL)
+	{
+		p->left = generate(key, value);
+		return true;
+	}
+	if (key < p->key && p->left != NULL)
+	{
+		myadd(p->left, key, value);
+	}
+	
+	//Rigth再帰
+	if (key < p->key && p->right == NULL)
+	{
+		p->right = generate(key, value);
+		return true;
+	}
+	if (key < p->key && p->left != NULL)
+	{
+		myadd(p->right, key, value);
+	}
+}
+
 // keyの値を見てノードを追加する
 bool add(tree* t, int key, const char* value)
 {
@@ -69,41 +106,43 @@ bool add(tree* t, int key, const char* value)
 	}
 
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
-	node* x=t->root;
-	while (1)
+	return myadd(t->root, key, value);
+}
+
+node* myfind(node* p, int key)
+{
+	if (key < p->key && p->left != NULL)
 	{
-		if (key == x->key)
-		{
-			x->key = key;
-			//x->value = value;
-			break;
-		}
-		if (key > x->key)
-		{
-			x = x->right;
-		}
-		if (key < x->key)
-		{
-			x = x->left;
-		}
-		if (x == NULL)
-		{
-			x = p;
-			break;
-		}
+		p->left = myfind(p->left, key);
 	}
-	return true;
+	if (key < p->key && p->left == NULL)
+	{
+		return NULL;
+	}
+
+	if (key >= p->key && p->right != NULL)
+	{
+		p->left = myfind(p->right, key);
+	}
+	if (key < p->key && p->right == NULL)
+	{
+		return NULL;
+	}
+
+	return p;
 }
 
 // keyの値を見てノードを検索して、値を取得する
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
-	//if (t->root->key > key)
-	
-	return NULL;
+	if (t == NULL || key == NULL || t->root == NULL)return NULL;
 }
 
+void mysearch()
+{
+
+}
 // keyの小さな順にコールバック関数funcを呼び出す
 void search(const tree* t, void (*func)(const node* p))
 {
