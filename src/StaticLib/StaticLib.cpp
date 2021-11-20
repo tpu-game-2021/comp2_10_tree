c7468466057d6c42e0f3a3a57c49cffdf38ae644
GTM106 <raito125raito@gmail.com>
2021-11-20 23:13:28 +0900

    メモリリークが治りませんでした

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -48,11 +48,35 @@ static node* generate(int key, const char* value)
 
 	p->key = key;
 	int n = (int)strlen(value);
-	memcpy(p->value, value, strlen(value)+1);
+	memcpy(p->value, value, strlen(value) + 1);
 
 	p->left = p->right = NULL;
 
 	return p;
+}
+
+static bool addnode(int key, const char* value, node* p)
+{
+	if (p == NULL) return false;
+
+	if (key < p->key) {
+		if (p->left == NULL) {
+			p->left = generate(key, value);
+			return (p->left != NULL);
+		}
+		return addnode(key, value, p->left);
+	}
+	else if (key > p->key) {
+		if (p->right == NULL) {
+			p->right = generate(key, value);
+			return (p->right != NULL);
+		}
+		return addnode(key, value, p->right);
+	}
+	else {
+		memcpy(p->value, value, sizeof(char) * 256);
+		return true;
+	}
 }
 
 // keyの値を見てノードを追加する
@@ -60,28 +84,55 @@ bool add(tree* t, int key, const char* value)
 {
 	if (t == NULL) return false;
 
-	node* p = generate(key, value);
-	if (p == NULL) return false;// メモリ確保できなかった。
-
 	if (t->root == NULL) {
-		t->root = p;
-		return true;
+		t->root = generate(key, value);
+		return  (t->root != NULL);// メモリ確保できたかどうか
 	}
 
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
+	return addnode(key, value, t->root);
+}
 
-	return true;
+static node* findnode(node* p, int key)
+{
+
+	if (key >= p->key) {
+		if (p->right == NULL)return NULL;
+		p->right = findnode(p->right, key);
+	}
+	else {
+		if (p->left == NULL)return NULL;
+		p->left = findnode(p->left, key);
+	}
+	return p;
 }
 
 // keyの値を見てノードを検索して、値を取得する
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
-	return NULL;
+	if (t == NULL || key == NULL)return NULL;
+	if (t->root == NULL)return NULL;
+
+	if (findnode(t->root, key)->value != NULL)
+		return findnode(t->root, key)->value;
+	else return NULL;
 }
 
+void searchkey(node* n, void (*func)(const node* p))
+{
+	if (n->left) {
+		searchkey(n->left, func);
+	}
+	func(n);
+	if (n->right) {
+		searchkey(n->right, func);
+	}
+}
 // keyの小さな順にコールバック関数funcを呼び出す
 void search(const tree* t, void (*func)(const node* p))
 {
 	// ToDo: 実装する
+	if (t == NULL)return;
+	searchkey(t->root, func);
 }
