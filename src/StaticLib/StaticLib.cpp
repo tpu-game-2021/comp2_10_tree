88fc136826cf21e7f08a7d4710f58a16ac8a1ab3
g2127018 <ababababa3045@gmail.com>
2021-11-22 00:58:34 +0900

    色々考えてみましたが、難しくて実装できませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -60,15 +60,34 @@ bool add(tree* t, int key, const char* value)
 {
 	if (t == NULL) return false;
 
+	//確保出来た場合
 	node* p = generate(key, value);
-	if (p == NULL) return false;// メモリ確保できなかった。
+
+	// メモリ確保できなかった場合。
+	if (p == NULL) return false;
+
+	//left、right、にアドレスが格納
+	//keyの値が"自分自身"よりも低い方がleft、大きいほうがright
+	//繋がるものが無ければ両方共null
+
+	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
+
+	//leftの代入
+	if (p->key > key)
+	{
+		t->root->left = p;
+	}
+
+	//rightの代入
+	if (p->key < key)
+	{
+		t->root->left = p;
+	}
 
 	if (t->root == NULL) {
 		t->root = p;
 		return true;
 	}
-
-	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
 
 	return true;
 }
@@ -76,6 +95,19 @@ bool add(tree* t, int key, const char* value)
 // keyの値を見てノードを検索して、値を取得する
 const char* find(const tree* t, int key)
 {
+	/*
+	//leftの代入
+	if ( > value)
+	{
+		t->root->left = p;
+	}
+
+	//rightの代入
+	if (p->key < key)
+	{
+		t->root->left = p;
+	}*/
+
 	// ToDo: 実装する
 	return NULL;
 }
