7d79476e5a867d7b24217f162f25316b22db8537
g2127043@st.t-kougei.ac.jp <g2127043@st.t-kougei.ac.jp>
2021-11-21 22:05:16 +0900

    テストを通せませんでした。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -17,12 +17,12 @@ static void release_recursive(node* n)
 {
 	if (n == NULL) return;
 
-	if (n->right) {
+	if (n->right != NULL) {
 		release_recursive(n->right);
 		n->right = NULL;
 	}
 
-	if (n->left) {
+	if (n->left != NULL) {
 		release_recursive(n->left);
 		n->left = NULL;
 	}
@@ -67,9 +67,32 @@ bool add(tree* t, int key, const char* value)
 		t->root = p;
 		return true;
 	}
-
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
+	node* q = t->root;
 
+	while (q) {
+		if (key == q->key) {
+			strcpy_s(q->value, 256, p->value);
+			free(p);
+			p = NULL;
+			break;
+		}
+		else if (key < q->key) {
+			if (q->left == NULL) {
+				q->left = p;
+				break;
+			}
+			q = q->left;
+		}
+		else if (key > q->key) {
+			if (q->right == NULL) {
+				q->right = p;
+				break;
+			}
+			q = q->right;
+		}
+		else return false;
+	}
 	return true;
 }
 
@@ -77,11 +100,48 @@ bool add(tree* t, int key, const char* value)
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
+	node* p;
+	p = t->root;
+	if (p == NULL) return NULL;
+
+	while(p){
+		if (key == p->key) {
+			return p->value;
+		}
+		else if (key < p->key) {
+			p = p->left;
+		}
+		else if (key > p->key) {
+			p = p->right;
+		}
+		else break;
+	}
 	return NULL;
 }
+
 
 // keyの小さな順にコールバック関数funcを呼び出す
 void search(const tree* t, void (*func)(const node* p))
 {
 	// ToDo: 実装する
+	int i = 0;
+	int flag = 0;
+
+	do{
+		node* p = t->root;
+		if (p == NULL) return;
+		flag = 0;
+		while (p) 
+		{
+			if (i == p->key) break;
+			else if (i < p->key) {
+				p = p->left;
+				flag = 1;
+			}
+			else if (i > p->key) p = p->right;
+		}
+		func(p);
+		i++;
+	} while (flag);
+	return;
 }
