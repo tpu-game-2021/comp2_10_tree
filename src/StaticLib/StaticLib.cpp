16c9e79644de9c3e45740f1aa4c572b3276fae61
shun-chi-man <shun-shun.2002-0505@ezweb.ne.jp>
2021-11-21 13:38:27 +0900

    テストを通るようにしました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -17,12 +17,14 @@ static void release_recursive(node* n)
 {
 	if (n == NULL) return;
 
-	if (n->right) {
+	if (n->right) 
+	{
 		release_recursive(n->right);
 		n->right = NULL;
 	}
 
-	if (n->left) {
+	if (n->left) 
+	{
 		release_recursive(n->left);
 		n->left = NULL;
 	}
@@ -63,25 +65,99 @@ bool add(tree* t, int key, const char* value)
 	node* p = generate(key, value);
 	if (p == NULL) return false;// メモリ確保できなかった。
 
-	if (t->root == NULL) {
+	if (t->root == NULL) 
+	{
 		t->root = p;
 		return true;
 	}
 
 	// Todo: t->rootの下にkeyの値の大小でleftかrightを切り替えながらpを追加する処理を実装する
 
-	return true;
+	node* attention = t->root;
+
+	while (1)
+	{
+		if (attention->key == p->key)
+		{
+			memcpy(attention->value, p->value, sizeof(char) * 256);
+			free(p);
+            return true;
+		}
+
+		else if (attention->key > p->key)
+		{
+			if (attention->left == NULL)
+			{
+				attention->left = p;
+				return true;
+			}
+
+			attention = attention->left;
+		}
+
+		else if (attention->key < p->key)
+		{
+			if (attention->right == NULL)
+			{
+				attention->right = p;
+				return true;
+			}
+
+			attention = attention->right;
+		}
+	}
 }
 
 // keyの値を見てノードを検索して、値を取得する
 const char* find(const tree* t, int key)
 {
 	// ToDo: 実装する
-	return NULL;
+    if (t->root == NULL)  return NULL;
+
+	node* attention = t->root;
+
+	while (1)
+	{
+		if (attention == NULL)  return NULL;
+
+		else
+		{
+			if (attention->key == key)
+			{
+				return (attention->value);
+			}
+
+			else if (attention->key > key)
+			{
+				attention = attention->left;
+			}
+
+			else if (attention->key < key)
+			{
+				attention = attention->right;
+			}
+		}
+	}
 }
 
 // keyの小さな順にコールバック関数funcを呼び出す
+void mysearch(node* attention, void (*func)(const node* p))
+{
+	if (attention->left)
+	{
+		mysearch(attention->left, func);
+	}
+
+	func(attention);
+
+	if (attention->right)
+	{
+		mysearch(attention->right, func);
+	}
+}
+
 void search(const tree* t, void (*func)(const node* p))
 {
 	// ToDo: 実装する
+	mysearch(t->root, func);
 }
